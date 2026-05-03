677116d7a28354a426a9d7fd2f14401097f1e827
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-05-03 23:29:31 +0300

    add filtering, sorting and URL validation

--- monitoring/serializers.py
+++ monitoring/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import MonitoredSite, ParseResult
 
 class MonitoredSiteSerializer(serializers.ModelSerializer):
+    def validate_url(self, value):
+        if not value.startswith("http://", "https://"):
+            raise serializers.ValidationError(
+                "URL must start with http:// or https://"
+            )
+        return value
+
     class Meta:
         model = MonitoredSite
         fields = '__all__'
